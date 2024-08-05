c50f57bbea848d9f60ca09c3e9cbe3401a8c6a97
Luis A Lopez Atencio <luisangellopezatencio@outlook.com>
2024-08-05 17:08:47 -0500

    Agregados todos los patrocinadores y todo el cronograma

--- src/index.js
+++ src/index.js
@@ -1,103 +1,102 @@
+// Variables de estado
 let respNav = true;
 let respMap = true;
+
+// Elementos del DOM
 const barraNavegacion = document.getElementById("barraNavegacion");
 const hamburguesa = document.getElementById("hamburguesa");
 const navegacion = document.getElementById("navegacion");
-const search = document.getElementById("search");
-const form = document.querySelector("form");
-const volverBtn = document.getElementById("volver-btn");
 const mapa = document.getElementById("map");
 const mostrarMapaBtn = document.getElementById("mostrarMapa-btn");
 const mostrarMapaIcon = document.getElementById("mostrarMapa-img");
-const ubicaciones = document.getElementsByClassName("ubicacion");
-const ubicacionSponsor1 = document.getElementById("ubicacion-sponsor1");
+const ubicacionesBtn = document.getElementsByClassName("ubicacion");
+const ubicacionSponsors = document.getElementsByClassName("mapSponsors")
 
+// Función para mostrar/ocultar la barra de navegación
 function mostrarNav() {
   if (respNav) {
     barraNavegacion.classList.remove("translate-x-[-220px]");
-    // Agregar transicion
-    barraNavegacion.classList.add("transition-all");
-    barraNavegacion.classList.add("duration-500");
+    barraNavegacion.classList.add("transition-all", "duration-500");
   } else {
     barraNavegacion.classList.add("translate-x-[-220px]");
   }
 }
 
-function MostrarMapa() {
+// Función para mostrar/ocultar el mapa
+function mostrarMapa() {
   if (respMap) {
     mapa.classList.add("h-[200px]");
     mapa.classList.remove("h-0");
-    // Agregar transicion
-    mapa.classList.add("transition-all");
-    mapa.classList.add("duration-500");
+    mapa.classList.add("transition-all", "duration-500");
   } else {
     mapa.classList.add("h-0");
     mapa.classList.remove("h-[200px]");
   }
 }
 
-function OcultarBarraNavegacion() {
+// Función para ocultar la barra de navegación
+function ocultarBarraNavegacion() {
   barraNavegacion.classList.add("translate-x-[-220px]");
 }
 
-function OcultarMostrarBuscar() {
+// Función para ocultar/mostrar la barra de navegación y formulario de búsqueda
+function ocultarMostrarBuscar() {
   navegacion.classList.toggle("hidden");
   form.classList.toggle("hidden");
   form.classList.toggle("flex");
 }
 
-function AnimarFlecheMapa(trigger) {
-    if(trigger){
-        mostrarMapaIcon.classList.toggle("rotate-180");
-    }
+// Función para animar el ícono de flecha del mapa
+function animarFlechaMapa(trigger) {
+  if (trigger) {
+    mostrarMapaIcon.classList.toggle("rotate-180");
+  }
 }
 
+// Event listeners
+
+// Toggle de la barra de navegación al hacer clic en el ícono de hamburguesa
 hamburguesa.addEventListener("click", () => {
   mostrarNav();
   navegacion.classList.toggle("hidden");
   respNav = !respNav;
 });
 
+// Ocultar barra de navegación al hacer clic fuera de la misma
 document.addEventListener("click", (e) => {
   if (!respNav && !e.target.closest("#hamburguesa")) {
-    OcultarBarraNavegacion();
+    ocultarBarraNavegacion();
     navegacion.classList.toggle("hidden");
     respNav = !respNav;
   }
 });
 
-search.addEventListener("click", () => {
-  OcultarMostrarBuscar();
-});
-
-volverBtn.addEventListener("click", () => {
-  OcultarMostrarBuscar();
-});
-
+// Fijar la barra de navegación al hacer scroll
 window.addEventListener("scroll", () => {
   const scrollPosicion = window.scrollY;
-  console.log(scrollPosicion);
   if (scrollPosicion > 640) {
-    navegacion.classList.add("fixed");
-    navegacion.classList.add("top-0");
+    navegacion.classList.add("fixed", "top-0", "bg-black");
     navegacion.classList.remove("mt-5");
-    navegacion.classList.add("bg-black");
   } else {
-    navegacion.classList.remove("fixed");
-    navegacion.classList.remove("top-0");
+    navegacion.classList.remove("fixed", "top-0", "bg-black");
     navegacion.classList.add("mt-5");
-    navegacion.classList.remove("bg-black");
   }
 });
 
+// Toggle del mapa al hacer clic en el botón correspondiente
 mostrarMapaBtn.addEventListener("click", () => {
-  MostrarMapa();
-  AnimarFlecheMapa(true);
+  mostrarMapa();
+  animarFlechaMapa(true);
   respMap = !respMap;
 });
 
-Array.from(ubicaciones).forEach((ubicacion) => {
-  ubicacion.addEventListener("click", () => {
-    ubicacionSponsor1.classList.toggle("h-0");
+// Toggle de la visibilidad del patrocinador al hacer clic en una ubicación
+Array.from(ubicacionesBtn).forEach((ubicacion, index) => {
+  ubicacion.addEventListener("click", (e) => {
+    const sponsorIndex = Math.floor(index / 2);
+    
+    if (sponsorIndex < ubicacionSponsors.length) {
+      ubicacionSponsors[sponsorIndex].classList.toggle("h-0");
+    }
   });
 });
